05bb880d79f6249fa0f7c9109b979ec02a459ea4
RaiYunan <yunanrai433@gmail.com>
2025-12-11 00:27:28 +0545

    Add optimized bubble sort implementation with swapped flag

--- src/SortingTechniques/BubbleSort.java
+++ src/SortingTechniques/BubbleSort.java
@@ -1,5 +1,38 @@
 package SortingTechniques;
 
 public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            // Optimization: stop if no swaps in first pass
+            boolean swapped = false;
+
+            for (int j = 0; j < n - 1 - i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // If no elements were swapped, array is already sorted
+            if (!swapped) break;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 4, 2};
+
+        bubbleSort(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
 
 }
