a6f8a07c407d3aa223a60a526a92160ac565a7f4
RaiYunan <yunanrai433@gmail.com>
2026-01-14 00:03:12 +0545

    add brute force aolution of lc 1283

--- src/ArrayPrograms/LC_1283_SmallestDivisor_BruteForce.java
+++ src/ArrayPrograms/LC_1283_SmallestDivisor_BruteForce.java
@@ -5,32 +5,25 @@ public class LC_1283_SmallestDivisor_BruteForce {
     // Brute force solution
     public static int smallestDivisor(int[] nums, int threshold) {
 
-        int max = getMax(nums);
-
-        // Try every possible divisor
-        for (int divisor = 1; divisor <= max; divisor++) {
-            int sum = 0;
-
-            // Compute sum using ceiling division
-            for (int num : nums) {
-                sum += (num + divisor - 1) / divisor;
-            }
-
-            // If condition satisfied, return divisor
-            if (sum <= threshold) {
-                return divisor;
-            }
-        }
-        return -1; // Problem guarantees an answer, so this won't occur
-    }
-
-    // Helper to find maximum value
-    private static int getMax(int[] nums) {
-        int max = Integer.MIN_VALUE;
+        int max = 0;
         for (int num : nums) {
             max = Math.max(max, num);
         }
-        return max;
+
+        // Try every possible divisor from 1 to max
+        for (int d = 1; d <= max; d++) {
+            int sum = 0;
+
+            for (int num : nums) {
+                sum += (num + d - 1) / d;  // ceil division
+            }
+
+            if (sum <= threshold) {
+                return d;  // smallest valid divisor
+            }
+        }
+
+        return -1; // will never happen due to constraints
     }
 
     // Driver code
