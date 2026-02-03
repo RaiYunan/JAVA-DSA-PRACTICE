3bb7f7e2268a92e2a47b7196c30575bd1cf09173
Yunan Rai <yunanrai433@gmail.com>
2026-02-03 12:57:29 +0545

    feat: add LC371 Sum of Two Integers (Iterative & Recursive)

--- src/BitManipulation/LC371_SumOfTwoIntegers.java
+++ src/BitManipulation/LC371_SumOfTwoIntegers.java
@@ -1,7 +1,8 @@
 package BitManipulation;
 
 public class LC371_SumOfTwoIntegers {
-    static int getSum(int a,int b){
+    //this is simple interview friendly solution #simple iterative approach
+    static int getSum1(int a,int b){
         while(b!=0){
             int carry=(a&b)<<1;
             a=a^b;
@@ -9,8 +10,16 @@ public class LC371_SumOfTwoIntegers {
         }
         return a;
     }
+
+    //this is optimal solution using recursion same as above;
+    static int getSum2(int a,int b){
+        if(b==0) return a;
+        return getSum2(a^b,(a&b)<<1);
+    }
+
     public static void main(String[] args){
         int a=5,b=3;
-        System.out.println(getSum(a,b));
+        System.out.println(getSum1(a,b));
+        System.out.println(getSum2(a,b));
     }
 }
