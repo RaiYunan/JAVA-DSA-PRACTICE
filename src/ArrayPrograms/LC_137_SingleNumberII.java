d6f4e0b2c580d0e78f58b7ea424f4e0bc6f1fbc6
Yunan Rai <yunanrai433@gmail.com>
2026-02-05 23:12:48 +0545

    feat: add 137 leetcode single number II solution

---
+++ src/ArrayPrograms/LC_137_SingleNumberII.java
@@ -0,0 +1,26 @@
+package ArrayPrograms;
+
+import java.util.Arrays;
+
+public class LC_137_SingleNumberII {
+
+        static int singleNumber(int[] nums) {
+            Arrays.sort(nums);
+            int n = nums.length;
+
+
+            for (int i = 0; i < n - 1; i += 3) {
+                if (nums[i] != nums[i + 1]) {
+                    return nums[i];
+                }
+            }
+
+            /
+            return nums[n - 1];
+        }
+
+        public static void main(String[] args){
+            int[] arr={2,2,1,2};
+            System.out.println(singleNumber(arr));
+        }
+}
