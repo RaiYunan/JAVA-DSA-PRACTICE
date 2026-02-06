d76eeb0fd2676f7399d6373c68a14f8a4e909329
Yunan Rai <yunanrai433@gmail.com>
2026-02-06 23:55:33 +0545

    feat: add new solution of single number II leetcdoe medium question

--- src/ArrayPrograms/LC_137_SingleNumberII.java
+++ src/ArrayPrograms/LC_137_SingleNumberII.java
@@ -4,6 +4,28 @@ import java.util.Arrays;
 
 public class LC_137_SingleNumberII {
 
+    static int singleNumber1(int[] nums) {
+        int result = 0;
+
+
+        for (int i = 0; i < 32; i++) {
+            int sum = 0;
+
+
+            for (int num : nums) {
+                if (((num >> i) & 1) == 1) {
+                    sum++;
+                }
+            }
+
+            if (sum % 3 != 0) {
+                result |= (1 << i);
+            }
+        }
+
+        return result;
+    }
+
         static int singleNumber(int[] nums) {
             Arrays.sort(nums);
             int n = nums.length;
@@ -22,5 +44,6 @@ public class LC_137_SingleNumberII {
         public static void main(String[] args){
             int[] arr={2,2,1,2};
             System.out.println(singleNumber(arr));
+            System.out.println(singleNumber1(arr));
         }
 }
