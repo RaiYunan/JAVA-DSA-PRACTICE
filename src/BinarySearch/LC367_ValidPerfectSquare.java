9d2e7857641f42765cd28620a5700ecf58313f8f
Yunan Rai <yunanrai433@gmail.com>
2026-02-08 23:59:54 +0545

    feat: Add optimla solution for lc 367

--- src/BinarySearch/LC367_ValidPerfectSquare.java
+++ src/BinarySearch/LC367_ValidPerfectSquare.java
@@ -12,8 +12,27 @@ public class LC367_ValidPerfectSquare {
         }
         return false;
     }
+    static boolean isPerfectSquare(int num) {
+        if (num < 1) return false;
+
+        long left = 1, right = num;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long square = mid * mid;
+
+            if (square == num) return true;
+            else if (square < num) left = mid + 1;
+            else right = mid - 1;
+        }
+        return false;
+    }
+
     public static void main(String[] args){
         System.out.println(isPerfectSquare_BruteForce(16));
         System.out.println(isPerfectSquare_BruteForce(14));
+
+        System.out.println(isPerfectSquare(16));
+        System.out.println(isPerfectSquare(14));
     }
 }
