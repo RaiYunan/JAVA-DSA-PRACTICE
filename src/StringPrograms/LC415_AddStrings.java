335ee5c41b2db451bcb2906ae5c5e10680d0df8a
Yunan Rai <yunanrai433@gmail.com>
2026-02-13 23:52:13 +0545

    feat: Add lc 415 new solution(Reverse Strings First)

--- src/StringPrograms/LC415_AddStrings.java
+++ src/StringPrograms/LC415_AddStrings.java
@@ -1,7 +1,7 @@
 package StringPrograms;
 
 public class LC415_AddStrings {
-    static String addStrings(String num1, String num2){
+    static String addStrings1(String num1, String num2){
         //Most common two-pointer method.
         StringBuilder res=new StringBuilder();
         int i=num1.length()-1;
@@ -21,10 +21,37 @@ public class LC415_AddStrings {
         }
         return res.reverse().toString();
     }
-     public static void main(String[] args){
+    // Time: O(n)
+// Space: O(n)
+
+        static String addStrings2(String num1, String num2) {
+            StringBuilder a = new StringBuilder(num1).reverse();
+            StringBuilder b = new StringBuilder(num2).reverse();
+            StringBuilder res = new StringBuilder();
+
+            int carry = 0;
+            int n = Math.max(a.length(), b.length());
+
+            for(int i = 0; i < n; i++) {
+                int d1 = i < a.length() ? a.charAt(i) - '0' : 0;
+                int d2 = i < b.length() ? b.charAt(i) - '0' : 0;
+
+                int sum = d1 + d2 + carry;
+                res.append(sum % 10);
+                carry = sum / 10;
+            }
+
+            if(carry != 0) res.append(carry);
+
+            return res.reverse().toString();
+        }
+
+
+    public static void main(String[] args){
         String s1="123";
         String s2="177";
-        System.out.println(addStrings(s1,s2));
+        System.out.println(addStrings1(s1,s2));
+        System.out.println(addStrings2(s1,s2));
 
     }
 }
