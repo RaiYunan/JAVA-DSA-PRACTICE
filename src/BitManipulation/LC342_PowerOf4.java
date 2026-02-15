0fb19040bb21bdde5b2cf16479d54974d223230e
Yunan Rai <yunanrai433@gmail.com>
2026-02-15 23:59:32 +0545

    feat: add new solution for power of four(LC 342)

--- src/BitManipulation/LC342_PowerOf4.java
+++ src/BitManipulation/LC342_PowerOf4.java
@@ -22,12 +22,22 @@ public class LC342_PowerOf4 {
         // Recursive case
         return isPowerOfFour2(n / 4);
     }
+    static boolean isPowerOfFour3(int n){
+        if (n <= 0) return false;
+
+        while (n % 4 == 0) {
+            n /= 4;
+        }
+
+        return n == 1;
+    }
     static void main(){
         int num1=16;
         System.out.println(isPowerOfFour1(num1));
         System.out.println(isPowerOfFour2(num1));
         int num2=8;
         System.out.println(isPowerOfFour2(num2));
+        System.out.println(isPowerOfFour3(num2));
     }
     }
 
