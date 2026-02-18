95efe8cf00ba3a520801cc0f131bc11794e9beb7
Yunan Rai <yunanrai433@gmail.com>
2026-02-19 00:01:29 +0545

    feat: add solution for reverse bits problem (LC190)

--- src/BitManipulation/LC190_ReverseBits.java
+++ src/BitManipulation/LC190_ReverseBits.java
@@ -2,16 +2,18 @@ package BitManipulation;
 
 public class LC190_ReverseBits {
 
-        public int reverseBits(int n) {
-            int result = 0;
-            for (int i = 0; i < 32; i++) {
-                result <<= 1;
-                result |= (n & 1);
-                n >>= 1;
-            }
-            return result;
+    public int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;
+            result |= (n & 1);
+            n >>= 1;
         }
-    void main(){
-        System.out.println(reverseBits(123456));
+        return result;
+    }
+
+    public static void main(String[] args) {
+        LC190_ReverseBits obj = new LC190_ReverseBits();
+        System.out.println(obj.reverseBits(123456));
     }
 }
