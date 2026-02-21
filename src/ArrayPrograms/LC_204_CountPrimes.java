31d7be7eeb995aefcbc535be1bf50ca59f29b05d
Yunan Rai <yunanrai433@gmail.com>
2026-02-21 23:03:53 +0545

    feat: add optimal solution for count primes(LC 204).

--- src/ArrayPrograms/LC_204_CountPrimes.java
+++ src/ArrayPrograms/LC_204_CountPrimes.java
@@ -2,7 +2,7 @@ package ArrayPrograms;
 
 public class LC_204_CountPrimes {
 
-        static int countPrimes(int n) {
+        static int countPrimes1(int n) {
             int count = 0;
 
             for(int i = 2; i < n; i++){
@@ -20,8 +20,30 @@ public class LC_204_CountPrimes {
         return true;
     }
 
+    public int countPrimes2(int n) { //optimal solution
+        if (n <= 2) return 0;
+
+        boolean[] isComposite = new boolean[n];
+
+        for (int i = 2; i * i < n; i++) {
+            if (!isComposite[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isComposite[j] = true;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (!isComposite[i]) count++;
+        }
+
+        return count;
+    }
+
         void main(){
             int n=10;
-            System.out.println(countPrimes(n));
+            System.out.println(countPrimes1(n));
+            System.out.println(countPrimes2(n));
         }
 }
