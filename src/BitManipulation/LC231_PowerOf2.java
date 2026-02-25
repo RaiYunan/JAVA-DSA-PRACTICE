46729812a59d02a8b6a383851071296429a579f0
Yunan Rai <yunanrai433@gmail.com>
2026-02-25 23:22:37 +0545

    feat: add solution of power of 2(LC 231)

--- src/BitManipulation/LC231_PowerOf2.java
+++ src/BitManipulation/LC231_PowerOf2.java
@@ -5,18 +5,23 @@ public class LC231_PowerOf2 {
     // A number is power of two if:
     // 1) n > 0
     // 2) it has exactly one set bit
-    static boolean isPowerOfTwo(int n) {
+    static boolean isPowerOfTwo1(int n) {
 
         // n & (n-1) removes the lowest set bit
         // For powers of two, result becomes 0
         return n > 0 && (n & (n - 1)) == 0;
+    }
+    static boolean isPowerOfTwo2(int n) {
+        if (n <= 0) return false;
+        while (n % 2 == 0) n /= 2;
+        return n == 1;
     }
 
     public static void main(String[] args) {
         int num1 = 128; // 10000000 -> true
         int num2 = 3;   // 00000011 -> false
 
-        System.out.println(isPowerOfTwo(num1));
-        System.out.println(isPowerOfTwo(num2));
+        System.out.println(isPowerOfTwo1(num1));
+        System.out.println(isPowerOfTwo2(num2));
     }
 }
