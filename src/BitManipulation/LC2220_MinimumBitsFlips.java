da46e91be01b8e3148ca8af471b9b2b9535e0a54
Yunan Rai <yunanrai433@gmail.com>
2026-03-03 23:15:46 +0545

    feat: add solution of minimum bit flips required.(LC 2220)

--- src/BitManipulation/LC2220_MinimumBitsFlips.java
+++ src/BitManipulation/LC2220_MinimumBitsFlips.java
@@ -1,7 +1,7 @@
 package BitManipulation;
 
 public class LC2220_MinimumBitsFlips {
-    static int minBitFlips(int start,int goal){
+    static int minBitFlips1(int start,int goal){
         int steps=start^goal;
         int count=0;
         while(steps!=0){
@@ -11,8 +11,20 @@ public class LC2220_MinimumBitsFlips {
         }
         return count;
     }
+    static int minBitFlips2(int start, int goal) {
+        int xor = start ^ goal;
+        int count = 0;
+
+        while (xor != 0) {
+            count += xor & 1;  // check last bit
+            xor >>= 1;         // shift right
+        }
+
+        return count;
+    }
     void main(){
         int start1=10,goal1=7;
-        System.out.printf("So %d steps is required to flip bits from %d to %d.",minBitFlips(start1,goal1),start1,goal1);
+        System.out.printf("So %d steps is required to flip bits from %d to %d.\n",minBitFlips1(start1,goal1),start1,goal1);
+        System.out.printf("So %d steps is required to flip bits from %d to %d.",minBitFlips2(start1,goal1),start1,goal1);
     }
 }
