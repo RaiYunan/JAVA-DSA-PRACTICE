2aaded2f4b084baca34a0243fbbc44aacb648d32
Yunan Rai <yunanrai433@gmail.com>
2026-03-07 00:02:41 +0545

    fix: add some fixes to the code

--- src/ArrayPrograms/LC_2_ShuffleTheArray.java
+++ src/ArrayPrograms/LC_2_ShuffleTheArray.java
@@ -3,19 +3,22 @@ package ArrayPrograms;
 import java.util.Arrays;
 
 public class LC_2_ShuffleTheArray {
-        public int[] shuffle(int[] nums, int n) {
-            int[] ans = new int[2 * n];
-            int index = 0;
 
-            for (int i = 0; i < n; i++) {
-                ans[index++] = nums[i];
-                ans[index++] = nums[i + n];
-            }
+    static int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[2 * n];
+        int index = 0;
 
-            return ans;
+        for (int i = 0; i < n; i++) {
+            ans[index++] = nums[i];
+            ans[index++] = nums[i + n];
         }
-    void main(){
-            int[] nums={2,5,1,3,4,7};
-        System.out.println(shuffle(Arrays.toString(nums)));
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,5,1,3,4,7};
+
+        System.out.println(Arrays.toString(shuffle(nums, nums.length / 2)));
     }
 }
