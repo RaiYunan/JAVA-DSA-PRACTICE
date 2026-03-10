f08b2f820b1d2f47ae1875a08a3e32d2e074b3ec
Yunan Rai <yunanrai433@gmail.com>
2026-03-10 21:18:05 +0545

    feat: add optimla solution for LC 509

--- src/Recursion/LC509_Fibonacci.java
+++ src/Recursion/LC509_Fibonacci.java
@@ -1,11 +1,24 @@
 package Recursion;
 
 public class LC509_Fibonacci {
-    static int fib1(int n){
+    static int fib1(int n){// Recursive approach but slower than iterative one
         if(n<=1) return n;
         return fib1(n-1)+fib1(n-2);
     }
+    static int fib2(int n){// Most optimal solution
+        if(n<=1) return n;
+        int a=0;
+        int b=1;
+        for(int i = 2; i <= n; i++){
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return b;
+    }
     void main(){
-        System.out.println(fib1(0));
+        System.out.println(fib1(4));
+        System.out.println(fib2(4));
     }
 }
