4029512dbd0c170447d5c5e5dca982a73129263f
Yunan Rai <yunanrai433@gmail.com>
2026-03-14 23:53:28 +0545

    feat: add optimal solution of Find Complement(LC 476).

--- src/BitManipulation/LC476_FindComplement.java
+++ src/BitManipulation/LC476_FindComplement.java
@@ -2,7 +2,7 @@ package BitManipulation;
 
 public class LC476_FindComplement {
 
-        public int findComplement(int num) {
+        public int findComplement1(int num) {//brute force solution
             int mask = 1;
 
             while(mask <= num){
@@ -12,8 +12,16 @@ public class LC476_FindComplement {
             return (mask - 1) ^ num;
         }
 
+        static int findComplement2(int num) {//optimal solution
+            int mask = Integer.highestOneBit(num);
+            mask = (mask << 1) - 1;
+            return num ^ mask;
+        }
+
+
         void main(){
             int n=5;
-            System.out.println(findComplement(n));
+            System.out.println(findComplement1(n));
+            System.out.println(findComplement2(n));
         }
 }
