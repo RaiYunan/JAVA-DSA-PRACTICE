5c9c7e7d6dbb5d34a31c02a110193d32880feed1
Yunan Rai <yunanrai433@gmail.com>
2026-03-20 23:55:39 +0545

    feat: add stack based optimal approach solution

--- src/StringPrograms/LC1021_RemoveOuterParentheses.java
+++ src/StringPrograms/LC1021_RemoveOuterParentheses.java
@@ -1,7 +1,7 @@
 package StringPrograms;
 
 public class LC1021_RemoveOuterParentheses {
-    static String removeOuterParentheses(String s) {
+    static String removeOuterParentheses1(String s) {
 
         StringBuilder result = new StringBuilder();
         int depth = 0;
@@ -24,8 +24,31 @@ public class LC1021_RemoveOuterParentheses {
 
         return result.toString();
     }
+
+        static String removeOuterParentheses2(String s) { //stack based approach
+
+            StringBuilder result = new StringBuilder();
+            int start = 0;
+            int balance = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+
+                if (s.charAt(i) == '(') balance++;
+                else balance--;
+
+
+                if (balance == 0) {
+                    result.append(s.substring(start + 1, i));
+                    start = i + 1;
+                }
+            }
+
+            return result.toString();
+
+    }
     void main(){
         String s="(()())(())";
-        System.out.println(removeOuterParentheses(s));
+        System.out.println(removeOuterParentheses1(s));
+        System.out.println(removeOuterParentheses2(s));
     }
 }
