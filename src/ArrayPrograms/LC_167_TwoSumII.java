f2e8a526cc758b9a67812e9913fb1b3570f0d12a
Yunan Rai <yunanrai433@gmail.com>
2026-04-03 22:55:26 +0545

    feat: add optimal solution for two sum ||.(LC 167)

--- src/ArrayPrograms/LC_167_TwoSumII.java
+++ src/ArrayPrograms/LC_167_TwoSumII.java
@@ -23,9 +23,31 @@ public class LC_167_TwoSumII {
 
         return new int[]{-1, -1};
     }
+
+    static int[] twoSumOptimal(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            }
+            else if (sum < target) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
     void main(){
     int[] nums={2,7,11,15};
     int target=17;
     System.out.println(Arrays.toString(twoSumBruteForce(nums,target)));
+        System.out.println(Arrays.toString(twoSumOptimal(nums,target)));
     }
 }
