590bd4988c5be2f5a2f76ded631e23e0ce6bce07
Yunan Rai <yunanrai433@gmail.com>
2026-04-17 23:37:36 +0545

    feat: add solution for post order traversal.(LC 145)

---
+++ src/BinaryTrees/LC145_BTPostOrderTraversal.java
@@ -0,0 +1,33 @@
+package BinaryTrees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC145_BTPostOrderTraversal {
+    static class TreeNode{
+        int val;
+       TreeNode left;
+        TreeNode right;
+        TreeNode(int val, TreeNode left,TreeNode right){
+            this.val=val;
+            this.left=left;
+            this.right=right;
+        }
+        static List<Integer> preorderTraversal(TreeNode root){
+            List<Integer> res=new ArrayList<>();
+            postorder(root,res);
+            return res;
+        }
+        static void postorder(TreeNode node, List<Integer> res){
+            if(node==null) return;
+
+
+            postorder(node.left,res);
+            postorder(node.right,res);
+            res.add(node.val);
+        }
+
+        void main(){
+
+        }
+}
