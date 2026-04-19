71b49f078d40d2d90a34e1f56141ace258f19d40
Yunan Rai <yunanrai433@gmail.com>
2026-04-19 20:50:59 +0545

    feat: minor errors fixed

--- src/BinaryTrees/LC145_BTPostOrderTraversal.java
+++ src/BinaryTrees/LC145_BTPostOrderTraversal.java
@@ -4,30 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LC145_BTPostOrderTraversal {
-    static class TreeNode{
+    static class TreeNode {
         int val;
-       TreeNode left;
+        TreeNode left;
         TreeNode right;
-        TreeNode(int val, TreeNode left,TreeNode right){
-            this.val=val;
-            this.left=left;
-            this.right=right;
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
         }
-        static List<Integer> preorderTraversal(TreeNode root){
-            List<Integer> res=new ArrayList<>();
-            postorder(root,res);
-            return res;
-        }
-        static void postorder(TreeNode node, List<Integer> res){
-            if(node==null) return;
+    }
+    static List<Integer> preorderTraversal(TreeNode root){
+        List<Integer> res=new ArrayList<>();
+        postorder(root,res);
+        return res;
+    }
+    static void postorder(TreeNode node, List<Integer> res){
+        if(node==null) return;
 
 
-            postorder(node.left,res);
-            postorder(node.right,res);
-            res.add(node.val);
-        }
+        postorder(node.left,res);
+        postorder(node.right,res);
+        res.add(node.val);
+    }
 
-        void main(){
+    void main(){
 
-        }
+    }
 }
