f87acac6cacd084238c9b96e40248fdbe46d7794
Yunan Rai <yunanrai433@gmail.com>
2026-04-25 23:58:31 +0545

    feat: add iterative stack based solution for preorder traversal.(LC 144)

--- src/BinaryTrees/LC144_BTPreorderTraversal.java
+++ src/BinaryTrees/LC144_BTPreorderTraversal.java
@@ -2,16 +2,15 @@ package BinaryTrees;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class LC144_BTPreorderTraversal {
-    static class TreeNode{
+    static class TreeNode {
         int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode(int val,TreeNode left,TreeNode right){
-            this.val=val;
-            this.left=left;
-            this.right=right;
+        TreeNode left, right;
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val; this.left = left; this.right = right;
         }
     }
     static List<Integer> preorderTraversal(TreeNode root){
@@ -27,7 +26,36 @@ public class LC144_BTPreorderTraversal {
         preorder(node.right,res);
     }
 
-    void main(){
+    //  Iterative Approach
+    static List<Integer> preorderIterative(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
 
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.add(node.val);
+            if (node.right != null) stack.push(node.right); // right first
+            if (node.left != null) stack.push(node.left);   // left on top
+        }
+
+        return res;
+    }
+    void main(){
+        // Build tree:     1
+        //                / \
+        //               2   3
+        //              / \
+        //             4   5
+        // Expected preorder: [1, 2, 4, 5, 3]
+        TreeNode root = new TreeNode(1,
+                new TreeNode(2, new TreeNode(4), new TreeNode(5)),
+                new TreeNode(3)
+        );
+
+        System.out.println( preorderTraversal(root));
+        System.out.println( preorderIterative(root));
     }
 }
