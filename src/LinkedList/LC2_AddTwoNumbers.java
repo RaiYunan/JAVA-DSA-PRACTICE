1105aca003a9ee6ae32de63dd6b2cea819fb1469
Yunan Rai <yunanrai433@gmail.com>
2026-05-01 19:16:07 +0545

    feat: add two numbers linked list solution (LC2)

--- src/LinkedList/LC2_AddTwoNumbers.java
+++ src/LinkedList/LC2_AddTwoNumbers.java
@@ -1,4 +1,65 @@
 package LinkedList;
 
 public class LC2_AddTwoNumbers {
+    static class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int val){
+            this.val=val;
+        }
+    }
+
+    static ListNode addTwoNumbers(ListNode l1,ListNode l2){
+        ListNode dummy=new ListNode(-1);
+        ListNode current=dummy;
+        int carry=0;
+
+        while(l1!=null || l2!=null || carry>0){
+            int sum=carry;
+
+            if(l1!=null){
+                sum+=l1.val;
+                l1=l1.next;
+            }
+
+            if(l2!=null){
+                sum+=l2.val;
+                l2=l2.next;
+            }
+
+            carry=sum/10;
+
+            int digit=sum%10;
+            current.next=new ListNode(digit);
+            current=current.next;
+        }
+        return dummy.next;
+    }
+
+    static ListNode build(int[] num){
+        ListNode dummy=new ListNode(0);
+        ListNode current=dummy;
+        for(int n:num){
+            current.next=new ListNode(n);
+            current=current.next;
+        }
+        return dummy.next;
+    }
+
+    static String printList(ListNode node){
+        StringBuilder sb = new StringBuilder();
+        while (node != null) {
+            sb.append(node.val).append(node.next != null ? " -> " : "");
+            node = node.next;
+        }
+        return sb.toString();
+    }
+    void main(){
+        ListNode l1 = build(new int[]{2, 4, 3});
+        ListNode l2 = build(new int[]{5, 6, 4});
+
+        System.out.println("Input  l1 : " + printList(l1));
+        System.out.println("Input  l2 : " + printList(l2));
+        System.out.println("Output    : " + printList(addTwoNumbers(l1, l2)));
+    }
 }
