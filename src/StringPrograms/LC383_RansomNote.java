83fe54d81b55edf8befe50d71f01aa688736983a
Yunan Rai <yunanrai433@gmail.com>
2026-07-13 19:57:59 +0545

    feat: add ransom note optimal solution (LC383)

--- src/StringPrograms/LC383_RansomNote.java
+++ src/StringPrograms/LC383_RansomNote.java
@@ -18,9 +18,20 @@ public class LC383_RansomNote {
         return true;
     }
 
+    static boolean canConstruct(String ransomNote, String magazine) {
+        int[] freq = new int[26];
+        for (char c : magazine.toCharArray())   freq[c - 'a']++;
+        for (char c : ransomNote.toCharArray()) if (--freq[c - 'a'] < 0) return false;
+        return true;
+    }
+
     void main() {
         System.out.println(canConstructBruteForce("a", "b"));    // false
         System.out.println(canConstructBruteForce("aa", "ab"));  // false
         System.out.println(canConstructBruteForce("aa", "aab")); // true
+
+        System.out.println(canConstruct("a", "b"));    // false
+        System.out.println(canConstruct("aa", "ab"));  // false
+        System.out.println(canConstruct("aa", "aab")); // true
     }
 }
