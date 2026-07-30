5c50b2d428aa1abaeb063b868f9b4e6bdfb241fc
Yunan Rai <yunanrai433@gmail.com>
2026-07-30 23:57:48 +0545

    feat: add backspace string compare stack solution (LC844)

--- src/StringPrograms/LC844_BackspaceStringCompare.java
+++ src/StringPrograms/LC844_BackspaceStringCompare.java
@@ -1,6 +1,9 @@
 package StringPrograms;
 
+import java.util.Stack;
+
 public class LC844_BackspaceStringCompare {
+
     /*
      * simulate typing with backspace '#' using a string builder
      * 1. iterate chars, append to sb if not '#'
@@ -10,25 +13,49 @@ public class LC844_BackspaceStringCompare {
      * string simulation (brute force)
      * O(n+m) time, O(n+m) space
      */
-    static String build(String str){
-        StringBuilder sb=new StringBuilder();
-        for(char ch: str.toCharArray()){
-            if(ch!='#'){
-                sb.append(ch);
-            }else if(!sb.isEmpty()){
-                sb.deleteCharAt(sb.length()-1);
-            }
-
+    static String build(String str) {
+        StringBuilder sb = new StringBuilder();
+        for (char ch : str.toCharArray()) {
+            if (ch != '#') sb.append(ch);
+            else if (!sb.isEmpty()) sb.deleteCharAt(sb.length() - 1);
         }
         return sb.toString();
     }
 
-    static boolean backspaceCompare(String s,String t){
+    static boolean backspaceCompareBrute(String s, String t) {
         return build(s).equals(build(t));
     }
-    void main(){
-        System.out.println(backspaceCompare("ab#c","ad#c"));    //true
-        System.out.println(backspaceCompare("ab##", "c#d#"));   // true
-        System.out.println(backspaceCompare("a#c", "b"));       // false
+
+    /*
+     * simulate typing using explicit stack, pop on '#'
+     * 1. push chars onto stack, pop on '#'
+     * 2. build string from stack, compare both
+     *
+     * stack simulation
+     * O(n+m) time, O(n+m) space
+     */
+    static String process(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : str.toCharArray()) {
+            if (ch == '#') { if (!stack.isEmpty()) stack.pop(); }
+            else             stack.push(ch);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char ch : stack) sb.append(ch);
+        return sb.toString();
+    }
+
+    static boolean backspaceCompare(String s, String t) {
+        return process(s).equals(process(t));
+    }
+
+    void main() {
+        System.out.println(backspaceCompareBrute("ab#c", "ad#c")); // true
+        System.out.println(backspaceCompareBrute("ab##", "c#d#")); // true
+        System.out.println(backspaceCompareBrute("a#c",  "b"));    // false
+
+        System.out.println(backspaceCompare("ab#c", "ad#c")); // true
+        System.out.println(backspaceCompare("ab##", "c#d#")); // true
+        System.out.println(backspaceCompare("a#c",  "b"));    // false
     }
 }
