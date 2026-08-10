2300f18036077e41a0fb36c305c97095310b66fd
Yunan Rai <yunanrai433@gmail.com>
2026-08-10 23:09:39 +0545

    feat: add find numbers with even number of digits solution (LC1295)

--- src/ArrayPrograms/LC1295_FindNumbersWithEvenNumberOfDigits.java
+++ src/ArrayPrograms/LC1295_FindNumbersWithEvenNumberOfDigits.java
@@ -1,4 +1,24 @@
 package ArrayPrograms;
 
 public class LC1295_FindNumbersWithEvenNumberOfDigits {
+
+    /*
+     * count numbers that have an even number of digits
+     * 1. check if number falls in even-digit ranges (2,4,6 digits)
+     * 2. given constraint 1 <= nums[i] <= 10^5, only 2 and 4 digit ranges apply
+     *
+     * range check
+     * O(n) time, O(1) space
+     */
+    static int findNumbers(int[] nums) {
+        int count = 0;
+        for (int num : nums)
+            if ((num >= 10 && num <= 99) || (num >= 1000 && num <= 9999)) count++;
+        return count;
+    }
+
+    void main() {
+        System.out.println(findNumbers(new int[]{12, 345, 2, 6, 7896})); // 2
+        System.out.println(findNumbers(new int[]{555, 901, 482, 1771})); // 1
+    }
 }
